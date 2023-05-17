f481f948e9016d346f9b51e8a336baf13069a1b5
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-05-17 22:42:20 +0100

    fixed bug when reading data file

--- data/relationships.py
+++ data/relationships.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 relationships = pd.read_excel('data/seating_data.xlsx')
 
+relationships.drop('idx', axis = 1, inplace = True)
+
 relationships_matrix = relationships.to_numpy()
