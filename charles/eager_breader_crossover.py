de7e35062abbace3c8a129f88254f0e07b018eda
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-05-17 23:04:35 +0100

    first commit of eager breader crossover

---
+++ charles/eager_breader_crossover.py
@@ -0,0 +1,95 @@
+from collections import Counter
+from copy import deepcopy
+from charles.charles import Individual
+
+def eager_breader_crossover(p1, p2):
+    """
+    Implementation of Eager Breader Crossover.
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individual: One offspring resulting from the crossover.
+        
+    """
+    
+    p1_idx = 0
+    p2_idx = 0
+
+    offspring = Individual(arrangement = None)
+
+    # Add tables until child has the correct number of tables
+    while(len(offspring) < len(p1)):
+        fit_table1 = p1.get_table_fitness(p1_idx)
+        fit_table2 = p2.get_table_fitness(p2_idx)
+
+        print('Fitness table 1: ', fit_table1)
+        print('Fitness table 2: ', fit_table2)
+
+        if fit_table1 >= fit_table2:
+            offspring.append_table(p1[p1_idx])
+            p1_idx += 1
+        else:
+            offspring.append_table(p2[p2_idx])
+            p2_idx += 1
+
+    # All goos until here
+        
+    # # ----------------- Repair phase ----------------- #
+
+    # # Count the occurrences of each number in the sets
+    # guest_counts = Counter(guest for table in offspring for guest in table)
+
+    # # Get the numbers that occur more than once
+    # repeated_guests = {guest for guest, count in guest_counts.items() if count > 1}
+
+    # # Remove guest from table where he has lowest fitness
+    # for guest in repeated_guests:
+    #     # Guest fitness in each table
+    #     guest_fitnesses = []
+    #     tables_idx = []
+
+    #     for table_idx, table in enumerate(offspring):
+    #         if guest in table:
+    #             guest_fitnesses.append(offspring.get_guest_fitness(guest, table_idx))
+    #             tables_idx.append(tables_idx)
+        
+    #     # Remove guest from the table where it has lowest fitness
+    #     if guest_fitnesses[0] >= guest_fitnesses[1]:
+    #         offspring[tables_idx[1]].remove(guest)
+    #     else:
+    #         offspring[tables_idx[0]].remove(guest)
+    
+    # # Fill the empty seats
+    # guests_per_table = len(p1[0])
+    # nr_guests = len(p1) * guests_per_table
+    # not_seated_guests = [guest for guest in range(1, nr_guests + 1) if guest not in guest_counts]
+
+    # for table_idx, table in enumerate(offspring):
+    #     # Table with empty seats
+    #     while len(table) != guests_per_table:
+    #         max_fitness = 0
+    #         guest_max_fitness = None
+    #         for guest in not_seated_guests:
+    #             # TODO: Não sei se é melhor maneira
+    #             off_aux = deepcopy(offspring)
+
+    #             # Seat guest at the table
+    #             off_aux.seat_guest(guest, table_idx)
+
+    #             # Check if guest has higher fitness in that table
+    #             if off_aux.get_guest_fitness(guest, table_idx) > max_fitness:
+    #                 max_fitness = off_aux.get_guest_fitness(guest, table_idx)
+    #                 guest_max_fitness = guest
+
+    #         # Seat the guest that has the highest fitness
+    #         offspring.seat_guest(guest_max_fitness, table_idx)
+
+    #         # Remove guest from the list of not seated guests
+    #         not_seated_guests.remove(guest_max_fitness)
+    
+    # return offspring
+
+        
