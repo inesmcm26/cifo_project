a112173dbe433b5139fb3fcdabe6c72c3a0b5c93
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-05-19 19:18:47 +0100

    sugestions of mutation

--- charles/mutation.py
+++ charles/mutation.py
@@ -1,31 +1,7 @@
 from random import randint, sample
 
-def binary_mutation(individual):
-    """
-    Binary mutation for a GA individual
 
-    Args:
-        individual (Individual): A GA individual from charles.py
-
-    Raises:
-        Exception: When individual is not binary encoded.py
-
-    Returns:
-        Individual: Mutated Individual
-    """
-    mut_index = randint(0, len(individual) - 1)
-
-    if individual[mut_index] == 0:
-        individual[mut_index] = 1
-    elif individual[mut_index] == 1:
-        individual[mut_index] = 0
-    else:
-        raise Exception(
-            f"Trying to do binary mutation on {individual}. But it's not binary.")
-        
-    return individual
-
-def swap_mutation(individual):
+def non_adjacent_swap_mutation(individual):
     """
     Swap mutation for a GA individual
 
@@ -33,34 +9,31 @@ def swap_mutation(individual):
 
     Returns: Individual: Mutated Individual
     """
+    # TODO
+    # Select two random tables
+    # Select n random individuals -> swap them
     mutation_idxs = sample(range(len(individual)), 2)
 
     individual[mutation_idxs[0]], individual[mutation_idxs[1]] = individual[mutation_idxs[1]], individual[mutation_idxs[0]]
 
     return individual
 
-def inversion_mutation(individual):
+def merge_and_split(individual):
+    # TODO
     """
-    Inversion mutation for a GA individual
-
-    Args: individual (Individual): A GA individual from charles.py
-
-    Returns: Individual: Mutated Individual
+    The Merge and Split, also known as Division and Combination operator,
+    works in two phases. In the first stage, it selects two groups and transforms
+    them into a single one. Then, in the second stage, it picks a group to distribute
+    its items between two distinct groups.
     """
-    mutation_idxs = sample(range(len(individual)), 2)
-
-    mutation_idxs.sort()
-
-    individual[mutation_idxs[0]:mutation_idxs[1]] = individual[mutation_idxs[0]:mutation_idxs[1]][::-1]
-
     return individual
 
+def chega_pra_la(individual):
+    # TODO
+    """
+    The Chega Pra Lá operator, also known as the shift operator, shifts everyone in the tables one
+    seat to the left or to the right.
+    
+    """
+    return individual
 
-if __name__ == '__main__':
-    # test = [0, 0, 0, 0]
-    # test = binary_mutation(test)
-
-    ind = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # ind = swap_mutation(ind)
-    ind = inversion_mutation(ind)
-    print(ind)
